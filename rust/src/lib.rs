982f9ff70d576cc17db24ca63e40be2d4a862ef3
Eleftherios Zisis <eleftherios.zisis@epfl.ch>
2024-11-14 20:39:49 +0100

    Changes

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,15 +1,87 @@
-use numpy::ndarray::{Array3, ArrayView1, ArrayView3};
-use numpy::{IntoPyArray, PyArray1, PyArray3, PyReadonlyArray1, PyReadonlyArray3};
+use rand::{Rng};
+
+use numpy::ndarray::{Array2, Array3, ArrayView1, ArrayView3};
+use numpy::{IntoPyArray, PyArray1, PyArray2,  PyArray3, PyReadonlyArray1, PyReadonlyArray3};
 use pyo3::{pymodule, types::PyModule, PyResult, Python, Bound};
+
+
+fn choose_voxel_index(
+    uniform_value: f64,
+    cumulative_probabilities: &Vec<f64>,
+    voxel_indices: &Vec<usize>
+) -> usize {
+
+    let index = cumulative_probabilities.binary_search_by(
+        |p| p.partial_cmp(&uniform_value).unwrap_or(std::cmp::Ordering::Greater)
+    );
+
+    match index {
+        Ok(i) => return i,
+        Err(_) => return 0
+    }
+
+}
 
 
 fn uniform(
     voxel_dimensions: ArrayView1<'_, f64>,
     density: ArrayView3<'_, f64>,
     offset: ArrayView1<'_, f64>,
-) -> Array3<f64>{
+) -> Array2<f64>{
     println!("{:?}", density.len());
-    &density + 1.0
+
+    let factor = 1e-9 * voxel_dimensions.iter().copied().product::<f64>();
+
+    let (nonzero_voxel_indices, nonzero_voxel_counts): (Vec<usize>, Vec<f64>)= density
+        .iter()
+        .enumerate()
+        .filter(|&(_, &value)| value > 0.0)
+        .map(|(i, value)| (i, value * factor) )
+        .unzip();
+
+    let total_count: f64 = nonzero_voxel_counts.iter().sum();
+
+    let cumulative_probabilities = nonzero_voxel_counts
+        .iter()
+        .scan(0.0, |sum, &count| {
+                *sum += count / total_count;
+                Some(*sum)
+            }
+        )
+        .collect();
+
+    let mut rng = rand::thread_rng();
+
+    let n_positions = total_count.round() as usize;
+
+    let nx = density.shape()[0];
+    let ny = density.shape()[1];
+    let nz = density.shape()[2];
+
+    let mut positions = Array2::<f64>::zeros((n_positions, 3));
+
+    for index in 0..n_positions {
+
+        let uniform_sample = rng.gen();
+
+        let voxel_index = choose_voxel_index(
+            uniform_sample,
+            &cumulative_probabilities,
+            &nonzero_voxel_indices,
+        );
+
+        let i: usize = voxel_index % &nx;
+        let j: usize = ((voxel_index - &i) / &nx ) % &ny;
+        let k: usize = ((voxel_index - &i) / &nx - &j) / &ny; 
+        
+        positions[(index, 0)] = &offset[0] + (i as f64) + rng.gen::<f64>();
+        positions[(index, 1)] = &offset[1] + (j as f64) + rng.gen::<f64>();
+        positions[(index, 2)] = &offset[2] + (k as f64) + rng.gen::<f64>();
+
+    }
+
+    positions
+
 }
 
 
@@ -24,7 +96,7 @@ fn _algorithms_impl<'py>(m: &Bound<'py, PyModule>) -> PyResult<()> {
         voxel_dimensions: PyReadonlyArray1<'py, f64>,
         density: PyReadonlyArray3<'py, f64>,
         offset: PyReadonlyArray1<'py, f64>,
-    ) -> Bound<'py, PyArray3<f64>> {
+    ) -> Bound<'py, PyArray2<f64>> {
         uniform(
             voxel_dimensions.as_array(),
             density.as_array(),
